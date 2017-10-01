50894335592c9104e3ff8b17505baf294edd1e0a
syedhasnatshah <13080819-016@uog.edu.pk>
2017-10-01 00:23:44 -0700

    Project Completed!.

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,7 +1,14 @@
 package com.londonappbrewery.magiceightball;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView ballDisplay=(ImageView) findViewById(R.id.image_eightBall);
+        final int [] array_for_referenc={R.drawable.ball1,R.drawable.ball2,R.drawable.ball3,R.drawable.ball4,R.drawable.ball5};
+        final Button myButton=( Button) findViewById(R.id.askButton);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random RandomNumberGenerator= new Random(); /* Here We are creating an object of class Random*/
+                int number=RandomNumberGenerator.nextInt(5); /* Now you are calling a class function using object of that class and assigning the return value to ineteger variable */
+                ballDisplay.setImageResource(array_for_referenc[number]);
+                Log.d("Pressed"," Number is "+number);
+            }
+        });
     }
 }
